d49f0d6ae01aaf72b884889e6741cf14648de572
hjkltyu <116849487+hjkltyu@users.noreply.github.com>
2025-02-17 17:53:19 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
-const UUID = process.env.UUID || '490ade06-6bba-440f-819a-acfcb895e2bf'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '23d706a8-9493-402d-b4bc-8de1b0c2c2f3'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.ghjdfgerf.tk';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMmE5ZTg4MjQzOWRiN2Q3ODViZTc1ODFhZDg3Nzk3NzIiLCJ0IjoiMzM4OGJiYWQtZTI3NS00NzZkLThhNDMtNWNkOWQ5Y2QzODFkIiwicyI6IlpEZzRaalkwWkdVdFpETTJPQzAwT1RZM0xXSXpPR010WkdabU5tSmxaRFJpWXpOaSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo-nb.reposit.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZTMyMjBhYjU1MTNiMTk4NDYwZjY3OTMwZmY5OTIyMWUiLCJ0IjoiZjQwNDgzNDItZGVjMi00ZTRlLThlODYtYzJjYjAyODAyNDI3IiwicyI6Ik5UWTFZVFpsWkdRdE5HTmpNUzAwTlRFd0xUZzNNV1l0Wldaa01qZzNNakkyT1daayJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
